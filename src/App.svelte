11cd6bfe92d01b7a2391d1f4de35dc21d4cb6f41
Terence Qu <therealterencequ@gmail.com>
2022-06-01 23:03:26 +1200

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,47 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+
+<script lang="ts">
+  import harry1 from "./assets/harry1.jpg";
+  import harry2 from "./assets/harry1.jpg";
+</script>
+
+<main>
+  <img class="defaultImage" src="{harry1}" alt="Harry 1"/>
+  <h1>Super Cheap Whoretos</h1>
+  <p>Harry Adams, Professional Whore</p>
+</main>
+
+<!-- Global styling -->
+<style global>
+  * {
+    font-family: "Comic Sans MS", cursive, sans-serif;
+  }
+  h1 {
+    font-size: 3em;
+    font-weight: bold;
+    margin: 0;
+  }
+  h2 {
+    font-size: 2em;
+    font-weight: normal;
+    margin: 0;
+  }
+  h3 {
+    font-size: 1.5em;
+    font-weight: bold;
+    margin: 0;
+  }
+  h4 {
+    font-size: 1.2em;
+    font-weight: bold;
+    margin: 0;
+  }
+  p {
+    font-family: 'Roboto', sans-serif;
+  }
+  .defaultImage {
+    width: 300px;
+  }
+</style>
