c7f4a574cbe7efb2a8536dac0e9853e9cb346714
Terence Qu <therealterencequ@gmail.com>
2022-06-01 23:12:50 +1200

    Static web app config and more styling

--- src/App.svelte
+++ src/App.svelte
@@ -10,38 +10,53 @@
 <main>
   <img class="defaultImage" src="{harry1}" alt="Harry 1"/>
   <h1>Super Cheap Whoretos</h1>
-  <p>Harry Adams, Professional Whore</p>
+  <p class="subtitle">Harry Adams, Professional Whore</p>
 </main>
 
 <!-- Global styling -->
 <style global>
+  /** MAIN LAYOUT */
+  body {
+    padding: 20px;
+    text-align: center;
+  }
+
+  /** TYPOGRAPHY */
   * {
-    font-family: "Comic Sans MS", cursive, sans-serif;
+    font-family: 'Comic Sans MS', sans-serif;
   }
   h1 {
     font-size: 3em;
     font-weight: bold;
-    margin: 0;
+    margin-bottom: 20px;
   }
   h2 {
     font-size: 2em;
     font-weight: normal;
-    margin: 0;
+    margin-bottom: 10px;
   }
   h3 {
     font-size: 1.5em;
     font-weight: bold;
-    margin: 0;
+    margin-bottom: 5px;
   }
   h4 {
     font-size: 1.2em;
     font-weight: bold;
-    margin: 0;
+    margin-bottom: 5px;
   }
   p {
     font-family: 'Roboto', sans-serif;
   }
+  .subtitle {
+    font-size: 1.2em;
+    font-weight: normal;
+    margin: 0;
+  }
+
+  /** IMAGES */
   .defaultImage {
     width: 300px;
   }
+
 </style>
