04233ed225614f153abc78283cd2b21bd22514ab
Terence Qu <therealterencequ@gmail.com>
2022-06-06 17:42:10 +1200

    Hopeful fix of harry-quest by hard injecting and refreshing JS script

--- src/pages/HarryQuest.svelte
+++ src/pages/HarryQuest.svelte
@@ -1,10 +1,44 @@
 <script>
+    import {onMount} from "svelte";
+
+    function injectScript(src) {
+        return new Promise((resolve, reject) => {
+            const id = "harry-quest-game-script";
+
+            // Clean up old script by refreshing the whole page
+            const oldScript = document.getElementById(id);
+            if (oldScript) {
+                console.log("hard refreshing page");
+                location.reload();
+            }
+
+            // Create new script
+            const script = document.createElement('script');
+            script.src = src;
+            script.id = id;
+            script.addEventListener('load', resolve);
+            script.addEventListener('error', e => reject(e.error));
+            document.head.appendChild(script);
+        });
+    }
+
+    async function startGameAsync(){
+        await injectScript('/games/harry-quest-game/game.js')
+            .then(() => {
+                console.log('Harry Quest JS script loaded!');
+            }).catch(error => {
+            console.error(error);
+        });
+    }
+
+    onMount(async () => {
+        await startGameAsync();
+    });
 </script>
 
 <main>
     <h1>Harry Quest</h1>
     <canvas id="canvas" width="276" height="414"></canvas>
-    <script type="text/javascript" src="/games/harry-quest-game/game.js" defer />
 </main>
 
 <style>
